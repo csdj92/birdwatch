03ec73ae98cbd151a1a561a1871796bc08972a90
csdj92 <csdj92@gmail.com>
2021-03-25 11:09:55 -0400

    pass in the top followers

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,10 @@ import React from 'react'
 import FollowerColumn from '../components/FollowersColumn'
 
 function Home() {
+    let topFiveFollowing
     return (
         <div className='container'>          
-                <FollowerColumn/>
+                <FollowerColumn users={topFiveFollowing}/>
                 <div className='feed'>  <h1>Home</h1></div>
                 <div className='suggested-box'>suggested</div>
         </div>
