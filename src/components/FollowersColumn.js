007b1b7a51315d1df475b516138994c988b83baf
csdj92 <csdj92@gmail.com>
2021-03-25 11:02:04 -0400

    map over users to display top 5

--- src/components/FollowersColumn.js
+++ src/components/FollowersColumn.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import UsersCard from './UsersCard'
 
-function FollowersColumn() {
+const FollowersColumn = (topFiveFollowing) => {
+        const users = topFiveFollowing.users
     return (
         <div className='followerscol'>
             <div className='followers-section'>
                 <div className="home"/>
                 <h2 className='bold-green'>For You</h2>
             </div>
+
+        <div className='followers-section'>
+            <div className='following'/>
+            <h2>Following</h2>
+        </div>
+
+        <hr/>
+
+        <p>Your Top Accounts</p>
+            {users && users.map(user =>(
+                <UsersCard key={user} user={user}
+                />
+            ))}
 
         </div>
     )
