32a15acded66daaae91b3d489ff5c1fd68c87be9
csdj92 <csdj92@gmail.com>
2021-03-25 11:02:27 -0400

    card for each top user

---
+++ src/components/UsersCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const UsersCard = ({user}) => {
+    return (
+        <div className='section-usercard'>
+        <img className='user-icon' src={user.avatar} width={'100%'} alt="i"/>
+        <div>
+            <h3 className='bold'>{user.username} </h3>
+                <p>{user.name}</p>
+        </div>
+            
+        </div>
+    )
+}
+
+
+
+export default UsersCard
