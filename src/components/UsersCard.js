a6b74de37f81c0475e622f59f3a2d7d94ca5793f
csdj92 <csdj92@gmail.com>
2021-04-02 10:50:52 -0400

    changed classname to be more consistent

--- src/components/UsersCard.js
+++ src/components/UsersCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 const UsersCard = ({user}) => {
     return (
         <div className='section-usercard'>
-        <img className='user-icon' src={user.avatar} width={'100%'} alt="i"/>
+        <img className='user-profile' src={user.avatar} width={'100%'} alt="i"/>
         <div>
             <h3 className='bold'>{user.username} </h3>
                 <p>{user.name}</p>
